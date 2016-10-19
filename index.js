4f9c65d61045a316ab70a6a4c37b970d6ec56995
Casey Foster <c@sey.me>
2016-10-18 20:30:03 -0500

    cogs@3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-module.exports = function (file, options, cb) {
-  var source = file.buffer.toString();
-  if (source.indexOf(options.errorText) > -1) return cb(new Error('No good!'));
-  cb(null, {buffer: new Buffer('bar\n')});
+module.exports = ({file: {buffer}, options}) => {
+  const source = buffer.toString();
+  if (source.indexOf(options.errorText) > -1) throw new Error('No good!');
+
+  return {buffer: new Buffer('bar\n')};
 };
--- test/test.js
+++ test/test.js
@@ -2,17 +2,7 @@ var helper = require('cogs-test-helper');
 
 helper.run({
   'test/config.json': {
-    'test/input.txt': {
-      path: 'test/input.txt',
-      buffer: helper.getFileBuffer('test/output.txt'),
-      hash: helper.getFileHash('test/output.txt'),
-      requires: [{
-        path: 'test/input.txt',
-        hash: helper.getFileHash('test/input.txt')
-      }],
-      links: [],
-      globs: []
-    },
+    'test/input.txt': helper.getFileBuffer('test/output.txt'),
     'test/error.txt': Error
   }
 });
